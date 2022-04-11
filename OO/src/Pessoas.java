f826b0d0a71c2b15b9efcf826ecc3f601516c44a
aluno <aluno@DSN1251017040>
2022-04-11 11:31:43 -0300

    commit 11.04

--- OO/src/Pessoas.java
+++ OO/src/Pessoas.java
@@ -3,9 +3,9 @@ public class Pessoas {
 //atributos	
 	String nome;
 	int numFig;//var global
-	
+	int numFigRecebe;
 //métodps	
-	void receber (int numFig){												//var local {
+	void receber (int pessoa1.numFigRecebe){												//var local {
 		this.numFig += numFig;																	//variavel global: todos os metodos dentro da classe enxergam					
 		//variavel local: existe apenas dentro de um metodo
 	}
--- OO/src/main.java
+++ OO/src/main.java
@@ -2,19 +2,37 @@ import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-	Pessoas pessoa1 = new Pessoas();
-	Scanner entrada = new Scanner (System.in);
-	Scanner enter = new Scanner(System.in);
-	//pessoa1.nome = "Emanuel";
-	System.out.println("Qual o nome da pessoa?");
-	pessoa1.nome = entrada.nextLine();
+//		Pessoas pessoa1 = new Pessoas(); //instanciando um obj
+//		
+//		pessoa1.nome = "Rafael";
+//		pessoa1.numFig = 4;
+//		
+//		pessoa1.receber(3);
+//		
+//		System.out.println(pessoa1.nome);
+//		System.out.println(pessoa1.numFig);
+//	}
+//}
+//	
+Pessoas pessoa1 = new Pessoas();
+Scanner entrada = new Scanner (System.in);
+Scanner enter = new Scanner(System.in);
 	
-	System.out.printf("Qual o número de figurinhas do(a) %s", pessoa1.nome);
-	pessoa1.numFig = enter.nextInt();
-	
-	System.out.printf("O número de cartas do(a) %s é: %d",pessoa1.nome, pessoa1.numFig);
-	//System.out.println(pessoa1.numFig);
-	
-	}
 
+System.out.println("Qual o nome da pessoa?");
+pessoa1.nome = entrada.nextLine();
+	
+System.out.printf("Qual o número de figurinhas do(a) %s", pessoa1.nome);
+pessoa1.numFig = enter.nextInt();
+
+System.out.printf("O número de cartas do(a) %s é: %d",pessoa1.nome, pessoa1.numFig);
+System.out.println(pessoa1.numFig);
+
+System.out.println("O número de cartas que ele vai receber é");
+pessoa1.numFigRecebe = entrada.nextInt();
+
+pessoa1.receber();
+	System.out.printf("O(a) %s recebe  figurinhas", pessoa1.nome);
 }
+}
+// da segunda forma nao da certo pq n puxa as informações certas
