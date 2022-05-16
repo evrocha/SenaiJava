c7ca3dbc27114e8b2bb0b207a2fbc3118606b883
evrocha <99438093+evrocha@users.noreply.github.com>
2022-05-16 08:57:50 -0300

    Update AreaCalculavel.java

--- Ex01-Interface/src/AreaCalculavel.java
+++ Ex01-Interface/src/AreaCalculavel.java
@@ -1,4 +1,4 @@
-
+// Interface AreaCalculavel. na Interface não há atributos ou implementação de códigos
 public interface AreaCalculavel {
 //	public void calcularArea(int a, String b, int c);
 	void calcularArea();
