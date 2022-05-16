e0a183ffc55f9e3a1154373c6fd00857e403de72
evrocha <99438093+evrocha@users.noreply.github.com>
2022-05-16 15:21:44 -0300

    Update main.java

--- FundamentosJava/src/main.java
+++ FundamentosJava/src/main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
+		
+				// criando variaveis de diversos tipos e mostrandos seu valor via sysdo
 //		int x= 100, a = 0;
 //		
 //		double y = 200.3, b =  0;
@@ -15,21 +17,23 @@ public class main {
 //		System.out.println(a);
 //		System.out.println(b);
 	
+				// neste bloco de codigos ha uma condiÃ§Ã£o que lÃª a idade de uma pessoa e, dependendo do valor, ha uma mensagem personalizada
 		//int idadePessoa=61;
 		//if(idadePessoa < 18) {
-			//System.out.println("Você não pode beber");
+			//System.out.println("VocÃª nÃ£o pode beber");
 		//}
 		//else if(idadePessoa < 60) {
 			//System.out.println("Permitido beber");
 		//}
 		//else {
-			//System.out.println("quanto é sua aposentadoria?");
+			//System.out.println("quanto Ã© sua aposentadoria?");
 		//}
-		//System.out.println("Você tem " + idadePessoa);
+		//System.out.println("VocÃª tem " + idadePessoa);
 		
 		
+			// bloco de codigos que, usando o switch case (escolha caso), manda uma mensagem diferente a cada dia da semana
 //		Scanner op = new Scanner(System.in);
-//		System.out.println("Digite qual é o dia da semana");
+//		System.out.println("Digite qual Ã© o dia da semana");
 //		int num = op.nextInt();
 //		switch(num) {
 //			case 1:
@@ -39,7 +43,7 @@ public class main {
 //				System.out.println("Segunda-feira");
 //				break;
 //			case 3:
-//				System.out.println("Terça-feira");
+//				System.out.println("TerÃ§a-feira");
 //				break;
 //			case 4:
 //				System.out.println("Quarta-feira");
@@ -51,25 +55,31 @@ public class main {
 //				System.out.println("Sexta-feira");
 //					break;	
 //			case 7:
-//				System.out.println("Sábado");
+//				System.out.println("SÃ¡bado");
 //					break;	
 //			default:
-//				System.out.println("Esse número não representa um dia da semana");
+//				System.out.println("Esse nÃºmero nÃ£o representa um dia da semana");
 //	}
 		
+		
+		// estrutra que le o caractere digitado e dependendo dele mostra uma mensagem diferente, usando o switch case
 //		Scanner op = new Scanner(System.in);
 //		char caractere = Integer.(op.nextLine());
 //	switch(caractere) {
 //	case 'a':
-//		System.out.println("Seu caractere é a");
+//		System.out.println("Seu caractere Ã© a");
 //		break;
 //case 'b':
-//		System.out.println("Seu caractere é b");
+//		System.out.println("Seu caractere Ã© b");
 //	break;
 //	default:
-//	System.out.println("Opção Inválida");
+//	System.out.println("OpÃ§Ã£o InvÃ¡lida");
 //}
 //	
+		
+		
+	// bloco de codigos que, usando o switch case (escolha caso),retorna uma mensagem diferente a cada farol de um semaforo
+
 //		
 //		Scanner op = new Scanner(System.in);
 //		
@@ -81,89 +91,92 @@ public class main {
 //			System.out.println("PARE");
 //			break;
 //		case "Amarelo":
-//			System.out.println("Atenção!!!");
+//			System.out.println("AtenÃ§Ã£o!!!");
 //			break;
 //		
 //		case "Verde":
 //			System.out.println("Siga!!!");
 //			break;
 //		default:
-//			System.out.println("Nenhuma opção disponível");
+//			System.out.println("Nenhuma opÃ§Ã£o disponÃ­vel");
 //		}
 		
+		
+		//  enquanto x for menor que  10, acrescerÃ¡ um em X e mostrara um SYSO disso
 //		while(x<10) {
 //			x ++;
 //			System.out.println(x);
 //		}
+		
+		
+		//  
 //		Scanner op = new Scanner(System.in);
-//		int x=555;
+//		x=555;
 //		int pss;
-//		System.out.println("Qual é a senha do cofre?");
+//		System.out.println("Qual Ã© a senha do cofre?");
+//
+		// vai executar esse bloco de comando antes de verificar a condiÃ§Ã£o
 //		do{
-//			System.out.println("Digite um número");
+//			System.out.println("Digite um nÃºmero");
 //			pss= op.nextInt();
 //			if(pss!=x){System.out.println("Senha incorreta");}
 //		}
 //		while(pss!=x);
 //			
 //		System.out.println("Senha correta");
-//		Scanner op = new Scanner(System.in);
+//		Scanner op1 = new Scanner(System.in);
 //		int op1 = op.nextInt();
+	
+		
+		// criando tres variaveis do tipo inteiro 
 		int x1 = 3;
 		int y1 = 5;
 		int z1 = 7;
-		
+		// exibindo na tela o valor das variaveis
 		System.out.println(x1);
 		System.out.println(y1);
 		System.out.println(z1);
 		System.out.println();
 		
+		// criando uma matriz X do tipo double com 4 indices
 		double x[] = {3,5,3,5};
 		
+		// laÃ§o de repetiÃ§Ã£o FOR
+		// enquanto a variavel inteira aux for menor que o tamanho da matriz, acresce um em aux
 		for (int aux = 0; aux < x.length; aux ++) {
-			System.out.println("Valor do indíce" + aux + " = "+ x[aux]);
+			System.out.println("Valor do indÃ­ce" + aux + " = "+ x[aux]);
 			}
+		// matriz de tipo String
 		String nome[] = {"Rafael", "Gislene", "Lara"};
 		
+		// basicamente a mesma condiÃ§Ã£o do laÃ§o anterior
 		for (int aux1 = 0; aux1 < nome.length; aux1++) {
 			System.out.println(nome[aux1]);
+			// se o nome contido em aux1 for gislene, retornara na tela a informaÃ§Ã£o
 			if (nome[aux1] == "Gislene"){
-				System.out.println("O indíce de Gislene é = "+ aux1);
+				System.out.println("O indÃ­ce de Gislene Ã© = "+ aux1);
 			}
 		
 		}
+		// criando 3 matrizes. tipo int, string e double
 		int [] arrayInt = new int[5];
 		String[] ArrayString = new String[4];
 		double[] ArrayDouble =new double[3];
 		
+		// a variavel 4 esta no terceiro indice
 		arrayInt[3] = 4;
 		
+		// neste bloco de codigos um valor int Ã© capturado via scanner a cada repetiÃ§Ã£o que for permitida, e no final hÃ¡ o retorno na tela
 		int[] numero = new int[6];
 		Scanner teclado = new Scanner(System.in);
 		
 		for(int aux2 =0; aux2 < numero.length; aux2++) {
-			System.out.println("Digite um número qualquer");
+			System.out.println("Digite um nÃºmero qualquer");
 		}
 		for (int aux2=0; aux2 <numero.length; aux2++) {
-			System.out.println("Indíce " + aux2 + " = "+ numero[aux2]);
+			System.out.println("IndÃ­ce " + aux2 + " = "+ numero[aux2]);
 		}
 	}
 }
-//		System.out.println("Qual número vai calcular?");
-//int op1 = op.nextInt();
-//	switch(op1) {
-//		case 1:
-//			for(int x=0, y=0; x<11;x=x+1, y= y+1) {
-//				System.out.println("1 x "+ y +" = "+ x);
-//				
-//			}
-//			break;
-//		case 2:
-//			for(int x=0, y=0; x<22;x=x+2, y= y+1) {
-//				System.out.println("2 x "+ y +" = "+ x);
-//				
-//			}		
-//			break;
-//	}
-//		
-sysehuifhwfejpo
+
+
