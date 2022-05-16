f9fe792332f516b466363735a3c0d520e4bd2396
evrocha <99438093+evrocha@users.noreply.github.com>
2022-05-16 14:33:31 -0300

    Add files via upload

---
+++ FundamentosJava/src/main.java
@@ -0,0 +1,169 @@
+import java.util.Scanner;
+public class main {
+
+	public static void main(String[] args) {
+//		int x= 100, a = 0;
+//		
+//		double y = 200.3, b =  0;
+//		short z = (short)100000;
+//		System.out.println(z);
+//		a =(int) y;
+//		b = x;
+//		
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(a);
+//		System.out.println(b);
+	
+		//int idadePessoa=61;
+		//if(idadePessoa < 18) {
+			//System.out.println("Você não pode beber");
+		//}
+		//else if(idadePessoa < 60) {
+			//System.out.println("Permitido beber");
+		//}
+		//else {
+			//System.out.println("quanto é sua aposentadoria?");
+		//}
+		//System.out.println("Você tem " + idadePessoa);
+		
+		
+//		Scanner op = new Scanner(System.in);
+//		System.out.println("Digite qual é o dia da semana");
+//		int num = op.nextInt();
+//		switch(num) {
+//			case 1:
+//				System.out.println("Domingo" );
+//				break;
+//			case 2:
+//				System.out.println("Segunda-feira");
+//				break;
+//			case 3:
+//				System.out.println("Terça-feira");
+//				break;
+//			case 4:
+//				System.out.println("Quarta-feira");
+//				break;
+//			case 5:
+//			System.out.println("Quinta-feira");
+//				break;	
+//			case 6:
+//				System.out.println("Sexta-feira");
+//					break;	
+//			case 7:
+//				System.out.println("Sábado");
+//					break;	
+//			default:
+//				System.out.println("Esse número não representa um dia da semana");
+//	}
+		
+//		Scanner op = new Scanner(System.in);
+//		char caractere = Integer.(op.nextLine());
+//	switch(caractere) {
+//	case 'a':
+//		System.out.println("Seu caractere é a");
+//		break;
+//case 'b':
+//		System.out.println("Seu caractere é b");
+//	break;
+//	default:
+//	System.out.println("Opção Inválida");
+//}
+//	
+//		
+//		Scanner op = new Scanner(System.in);
+//		
+//		System.out.println("Qual o farol?");
+//		String farol = op.nextLine();
+//		
+//		switch (farol) {
+//		case "Vermelho":
+//			System.out.println("PARE");
+//			break;
+//		case "Amarelo":
+//			System.out.println("Atenção!!!");
+//			break;
+//		
+//		case "Verde":
+//			System.out.println("Siga!!!");
+//			break;
+//		default:
+//			System.out.println("Nenhuma opção disponível");
+//		}
+		
+//		while(x<10) {
+//			x ++;
+//			System.out.println(x);
+//		}
+//		Scanner op = new Scanner(System.in);
+//		int x=555;
+//		int pss;
+//		System.out.println("Qual é a senha do cofre?");
+//		do{
+//			System.out.println("Digite um número");
+//			pss= op.nextInt();
+//			if(pss!=x){System.out.println("Senha incorreta");}
+//		}
+//		while(pss!=x);
+//			
+//		System.out.println("Senha correta");
+//		Scanner op = new Scanner(System.in);
+//		int op1 = op.nextInt();
+		int x1 = 3;
+		int y1 = 5;
+		int z1 = 7;
+		
+		System.out.println(x1);
+		System.out.println(y1);
+		System.out.println(z1);
+		System.out.println();
+		
+		double x[] = {3,5,3,5};
+		
+		for (int aux = 0; aux < x.length; aux ++) {
+			System.out.println("Valor do indíce" + aux + " = "+ x[aux]);
+			}
+		String nome[] = {"Rafael", "Gislene", "Lara"};
+		
+		for (int aux1 = 0; aux1 < nome.length; aux1++) {
+			System.out.println(nome[aux1]);
+			if (nome[aux1] == "Gislene"){
+				System.out.println("O indíce de Gislene é = "+ aux1);
+			}
+		
+		}
+		int [] arrayInt = new int[5];
+		String[] ArrayString = new String[4];
+		double[] ArrayDouble =new double[3];
+		
+		arrayInt[3] = 4;
+		
+		int[] numero = new int[6];
+		Scanner teclado = new Scanner(System.in);
+		
+		for(int aux2 =0; aux2 < numero.length; aux2++) {
+			System.out.println("Digite um número qualquer");
+		}
+		for (int aux2=0; aux2 <numero.length; aux2++) {
+			System.out.println("Indíce " + aux2 + " = "+ numero[aux2]);
+		}
+	}
+}
+//		System.out.println("Qual número vai calcular?");
+//int op1 = op.nextInt();
+//	switch(op1) {
+//		case 1:
+//			for(int x=0, y=0; x<11;x=x+1, y= y+1) {
+//				System.out.println("1 x "+ y +" = "+ x);
+//				
+//			}
+//			break;
+//		case 2:
+//			for(int x=0, y=0; x<22;x=x+2, y= y+1) {
+//				System.out.println("2 x "+ y +" = "+ x);
+//				
+//			}		
+//			break;
+//	}
+//		
+sysehuifhwfejpo
