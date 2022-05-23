125b9f328f3cfd5896623f078de80eb2ae364865
evrocha <99438093+evrocha@users.noreply.github.com>
2022-05-23 18:56:44 -0300

    Update main.java

--- ExercicioLampada/src/main.java
+++ ExercicioLampada/src/main.java
@@ -4,37 +4,38 @@ public class main {
 	public static void main(String[] args) {
 		
 		
-		
+		// variavel q ja inicia falsa a condi√ß√£o ligada
 		Lampada l1 = new Lampada(false);
 		
 		Scanner in = new Scanner(System.in);
 		
-		System.out.println("A lampada est· " + l1.imprimir());
+		// usa o metodo imprimir para mostrar se ligada ou desligada
+		System.out.println("A lampada est√° " + l1.imprimir());
 		System.out.println();
 		
-		
+		// bloco q testa a resposta fornecida e a usa para alterar a  lampada 
 		System.out.println("deseja ligar a lampada?");;
 		String lol = in.nextLine();
 		
 		if( (lol.equals("sim"))) {
 		l1.ligar();
-		System.out.println("Agora ela est·: " + l1.imprimir());
+		System.out.println("Agora ela est√°: " + l1.imprimir());
 		System.out.println();
 		System.out.println( "Deseja desligar ela novamente?");
 		String lo = in.nextLine();
 		
 	    if( (lo.equals("sim"))) {
 		l1.desligar();
-		System.out.println("Agora ela est·: " + l1.imprimir());
+		System.out.println("Agora ela est√°: " + l1.imprimir());
 		}
 		
 		}else if( (lol.equals("nao"))) { 
 			l1.ligar();
-			System.out.println("Ent„o ela ir· continuar: " + l1.imprimir());
-		System.out.println("Agora ela est·: " + l1.imprimir());
+			System.out.println("Ent√£o ela ir√° continuar: " + l1.imprimir());
+		System.out.println("Agora ela est√°: " + l1.imprimir());
 	}else if( (lol.equals("nao"))) { 
 		l1.desligar();
-		System.out.println("Ent„o ela ir· continuar: " + l1.imprimir());
+		System.out.println("Ent√£o ela ir√° continuar: " + l1.imprimir());
 		
 		}
 	}
