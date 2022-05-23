afbaf7e942269681b62b1eda314a30ceb48f3b22
evrocha <99438093+evrocha@users.noreply.github.com>
2022-05-23 14:50:28 -0300

    Add files via upload

---
+++ MediaAluno/src/Aluno.java
@@ -0,0 +1,21 @@
+
+public class Aluno{
+
+	
+		
+		Nota nota;
+		double media;
+		double n1;
+		double n2;
+		double n3;
+		
+		void Media(double n1, double n2, double n3) {
+			
+			media = (n1+n2+n3)/3;
+			
+		
+
+		}
+	}
+
+
---
+++ MediaAluno/src/Nota.java
@@ -0,0 +1,30 @@
+
+public class Nota {
+	
+	   double  nota1 = 8.5;
+	   double  nota2= 7.5;
+	   double  nota3 = 6.0;
+	   double  notat1;
+	   double  notat2;
+	   double  notat3;
+	   int peso1 = 3;
+	   int peso2 = 2;
+	   int peso3 = 5;   
+	   
+	   void calcular( double nota1, double nota2, double  nota3, int peso1,  int peso2, int peso3) {
+		   
+		   notat1 = nota1/10 * peso1;
+		   notat2 = nota2/10 * peso2;
+		   notat3 = nota3/10 * peso3;
+		  
+		   
+	   }
+	     
+	   
+	  
+	   
+	   
+	   
+	  
+
+}
