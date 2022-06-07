107af3ebbd4da3cc9c6deca92a35797922c5ff2c
evrocha <99438093+evrocha@users.noreply.github.com>
2022-06-07 07:46:24 -0300

    Update Apartamento.java

--- Interfaces/src/Apartamento.java
+++ Interfaces/src/Apartamento.java
@@ -1,6 +1,6 @@
 // apartamento implementa os metodos da interface ItemCaro.
 public class Apartamento implements ItemCaro {
-
+// override do meodo getPreco da interface
 	@Override
 	public double getPreco() {
 		// TODO Auto-generated method stub
