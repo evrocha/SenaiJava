8aa342bcce279cc5566c3a589a9f3cf8103fdcb0
evrocha <99438093+evrocha@users.noreply.github.com>
2022-06-07 07:43:54 -0300

    Update ItemCaro.java

--- Interfaces/src/ItemCaro.java
+++ Interfaces/src/ItemCaro.java
@@ -1,4 +1,4 @@
-
+// interface itemCaro. Nela contem os metodos que a classe que a implementar herdará
 public interface ItemCaro {
 	// um metodo que retorna o valor deste item caro em double
 	double getPreco();
