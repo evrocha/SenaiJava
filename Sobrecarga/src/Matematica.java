65e63ca44f48003bd28ede1f0f4fa175f7ca04d7
evrocha <99438093+evrocha@users.noreply.github.com>
2022-06-13 14:10:50 -0300

    Update Matematica.java

--- Sobrecarga/src/Matematica.java
+++ Sobrecarga/src/Matematica.java
@@ -1,16 +1,18 @@
 public class Matematica {
-
+// metodo somar recebe dois parametros inteiros
 	int somar(int x, int y) {
 	return x+y;
 	
 	}
 	
+// metodo somar recebe tres parametros
 	int somar(int x, int y, int z) {
 		
 		return x+y+z;
 		
 	}
 	
+// metodo somar recebe dois parametros em  double
 	double somar(double x, double y) {
 		
 		return x+y;
